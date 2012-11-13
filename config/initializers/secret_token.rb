11489137fefb13d4cbda1ebf8db9b7f07a303c7b
Li Ouyang <li.ouyang@gmail.com>
2012-11-13 10:10:44 -0500

    first real commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MTA307::Application.config.secret_token = '3d8fd722a7f0f7d70e88dd2a85b8f553eee337eb5aa667a1ee92cb270274913b50111a7a21ac578f6d15a677be8b80ac6a3b4bcb754695489a25bbb23e9e0b10'
